851d365623e6e4c5456a1c3cc04d8d1568734f65
Simon A. F. Lund <slund@cnexlabs.com>
2018-11-29 11:22:37 +0100

    envs: added export

--- envs/refenv-liblightnvm.sh
+++ envs/refenv-liblightnvm.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
-
-# Set device identifier
-#NVM_DEV_IDENT=/dev/nvme0n1
-#NVM_DEV_IDENT=traddr:0000:00:01.0
+#
+# liblightnvm
+#
+# - Choose device identifier based on NVM_CLI_BE_ID
+# - Optionally configure SPDK hugemem
+#
+# Set NVM device identifier
+export NVM_DEV_IDENT
+if [[ "$NVM_CLI_BE_ID" == "4" ]]; then
+  NVM_DEV_IDENT="traddr:$PCI_DEV_NAME"
+else
+  NVM_DEV_IDENT="/dev/$NVME_DEV_NAME"
+fi
+#
+# OPTIONAL: Change the memory assigned to SPDK
+#export HUGEMEM=8192
+#
