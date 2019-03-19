c157dcd4eb1cdc885c1343b6f5d588cc27dc53b7
Simon A. F. Lund <slund@cnexlabs.com>
2019-03-19 23:48:00 +0100

    version-bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import glob
 
 setup(
     name="cijoe-pkg-liblightnvm",
-    version="0.0.5",
+    version="0.0.6",
     description="CIJOE liblightnvm Package",
     author="Simon A. F. Lund",
     author_email="slund@cnexlabs.com",
