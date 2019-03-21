f2b649797c1638a1b69d54081e28d66381c71bc1
Simon A. F. Lund <slund@cnexlabs.com>
2019-03-21 15:35:58 +0100

    fixed reference configuration

--- envs/refenv-liblightnvm.sh
+++ envs/refenv-liblightnvm.sh
@@ -2,17 +2,30 @@
 #
 # liblightnvm
 #
-# - Choose device identifier based on NVM_CLI_BE_ID
-# - Optionally configure SPDK hugemem
+# The testcases explicitly forward the environment variable 'NVM_BE', the
+# following section assigns 'NVM_DEV_IDENT' based on the value of 'NVM_BE'.
 #
-# Set NVM device identifier
-export NVM_DEV_IDENT
-if [[ "$NVM_CLI_BE_ID" == "4" ]]; then
-  NVM_DEV_IDENT="traddr:$PCI_DEV_NAME"
-else
-  NVM_DEV_IDENT="/dev/$NVME_DEV_NAME"
-fi
+# NOTE:
+# - Change the memory assigned to SPDK via env. var. 'HUGEMEM' in testplan
 #
-# OPTIONAL: Change the memory assigned to SPDK
-#export HUGEMEM=8192
-#
+
+# liblightnvm; set NVM_DEV_IDENT
+case $NVM_BE in
+NVM_BE_IOCTL)
+  : ${NVM_DEV_IDENT:="/dev/$NVME_DEV_NAME"}
+  export NVM_DEV_IDENT
+  ;;
+NVM_BE_LBD)
+  : ${NVM_DEV_IDENT:="/dev/$NVME_DEV_NAME"}
+  export NVM_DEV_IDENT
+  ;;
+NVM_BE_SPDK)
+  : ${NVM_DEV_IDENT:="traddr:$PCI_DEV_NAME"}
+  export NVM_DEV_IDENT
+  ;;
+NVM_BE_NOCD)
+  : ${NVM_DEV_IDENT:="traddr:$PCI_DEV_NAME"}
+  export NVM_DEV_IDENT
+  ;;
+esac
+
