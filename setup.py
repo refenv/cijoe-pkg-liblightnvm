7d665583c3211f344137417a7cfdcb9ff5843103
Simon A. F. Lund <slund@cnexlabs.com>
2019-03-22 17:41:16 +0100

    dist: fixes

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
-from setuptools import setup
+"""
+    Definition of CIJOE/liblightnvm distribution package
+"""
 import glob
+from setuptools import setup
 
 setup(
     name="cijoe-pkg-liblightnvm",
     version="0.0.8",
-    description="CIJOE liblightnvm Package",
+    description="CIJOE: liblightnvm package",
     author="Simon A. F. Lund",
     author_email="slund@cnexlabs.com",
     url="https://github.com/safl/cijoe-pkg-liblightnvm",
     license="BSD-2",
     install_requires=[
-        "cijoe (>=0.0.14)"
+        "cijoe (>=0.0.16)"
     ],
     zip_safe=False,
     data_files=[
-         ("share/cijoe/envs", glob.glob("envs/*")),
+        ("share/cijoe/envs", glob.glob("envs/*")),
 
-         ("share/cijoe/testfiles", glob.glob("testfiles/*")),
-         ("share/cijoe/testcases", glob.glob("testcases/*")),
-         ("share/cijoe/testsuites", glob.glob("testsuites/*")),
-         ("share/cijoe/testplans", glob.glob("testplans/*"))
-    ]
+        ("share/cijoe/testfiles", glob.glob("testfiles/*")),
+        ("share/cijoe/testcases", glob.glob("testcases/*")),
+        ("share/cijoe/testsuites", glob.glob("testsuites/*")),
+        ("share/cijoe/testplans", glob.glob("testplans/*"))
+    ],
+    options={'bdist_wheel':{'universal':True}}
 )
